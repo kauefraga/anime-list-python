55ec0fac533630456d37b8c4b3c023a11f52b0f0
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-01-05 17:42:26 -0300

    :sparkles: feat: add verification and prettify with some icons

--- src/commands/open.py
+++ src/commands/open.py
@@ -1,17 +1,23 @@
 import click
 from rich import print
+from components.icons import Icon
 from infra.db import Anime, db
 
 @click.command()
 @click.argument('title')
 def open(title: str):
   """Search for an anime with a title and return it with a url"""
-  print('Title: [orange_red1]{}[/orange_red1]'.format(title))
+  print('{} Searching...'.format(Icon.INTERROGATIVE.value))
 
-  anime = Anime.get(Anime.title == title)
+  anime = Anime.get_or_none(Anime.title == title)
+
+  if anime is None:
+    print('{} [bold red]{} not found.[/bold red]'.format(Icon.MINUS.value, title))
+    exit(0)
 
   print('[bold green]Found![/bold green]')
 
-  print('{}'.format(anime.url))
+  print('{} Title: [orange_red1]{}[/orange_red1]'.format(Icon.PLUS.value, anime.title))
+  print('{} URL: [orange_red1]{}[/orange_red1]'.format(Icon.PLUS.value, anime.url))
 
   db.close()
