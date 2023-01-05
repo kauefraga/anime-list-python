463e689c166c6e3b456d611182c8f61b041856b3
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-01-05 16:29:49 -0300

    :sparkles: feat: add already exists verification and prettify with some icons

--- src/commands/save.py
+++ src/commands/save.py
@@ -1,5 +1,6 @@
 import click, validators, datetime
 from rich import print
+from components.icons import Icon
 from infra.db import Anime, db
 
 @click.command()
@@ -8,13 +9,21 @@ from infra.db import Anime, db
 def save(title: str, url: str):
   """Save an anime with a given title/description"""
   if not validators.url(url):
-    print('[bold red]The URL is not valid[/bold red]')
+    print('{} [bold red]The URL is not valid[/bold red]'.format(Icon.MINUS.value))
+    exit(0)
 
-  print('Title: [orange_red1]{}[/orange_red1]'.format(title))
-  print('URL: [orange_red1]{}[/orange_red1]'.format(url))
+  animeAlreadyExists = Anime.get_or_none(Anime.title == title)
+
+  if animeAlreadyExists:
+    print('{} [bold red]{} already exists[/bold red]'.format(Icon.MINUS.value, title))
+    exit(0)
+
+  print('{} Title: [orange_red1]{}[/orange_red1]'.format(Icon.PLUS.value, title))
+  print('{} URL: [orange_red1]{}[/orange_red1]'.format(Icon.PLUS.value, url))
 
   Anime.create(title=title, url=url, created_at=datetime.datetime.now())
 
+  print('[green]Done![/green]')
   print('[green]The anime {} is saved in the database[/green]'.format(title))
 
   db.close()
---
+++ src/components/icons.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Icon(Enum):
+  PLUS = '[green][[/green]+[green]][/green]'
+  MINUS = '[red][[/red]-[red]][/red]'
+  INTERROGATIVE = '[yellow][[/yellow]?[yellow]][/yellow]'
