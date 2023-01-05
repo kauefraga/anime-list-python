6f1dba8d70487c8ec0acd8b772f12f3a5bdeb7d0
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-01-05 14:09:09 -0300

    :recycle: chore: create a table, loop through the database and display

--- src/commands/list.py
+++ src/commands/list.py
@@ -1,10 +1,20 @@
 import click
+from rich import print
+from rich.table import Table
 from infra.db import Anime, db
 
 @click.command()
 def list():
   """Query and show all the saved animes"""
+  table = Table(title="Anime List")
+
+  table.add_column("Title", style="magenta")
+  table.add_column("Created at", justify="right", style="cyan", no_wrap=True)
+
   for anime in Anime.select().order_by(Anime.created_at.desc()):
-    print(anime.title, anime.created_at)
+    created_at, ms = anime.created_at.split('.')
+    table.add_row(anime.title, created_at)
+
+  print(table)
 
   db.close()
