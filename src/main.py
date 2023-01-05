d93f8043a9aab161205e31907fa3b3d47018fe6c
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-01-05 16:28:25 -0300

    :recycle: chore: add cli description

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@ from commands.open import open
 @click.version_option('0.1.0', message='%(prog)s version %(version)s')
 @click.group(context_settings={'help_option_names': ['-h', '--help']})
 def cli():
+  """
+    A list to save information about some animes\n
+      example: python src/main.py list
+  """
   pass
 
 if __name__ == '__main__':
