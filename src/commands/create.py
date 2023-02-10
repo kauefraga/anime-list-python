cb9d399d271522d6d480f6bc96992c7bc7376ee0
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-02-09 21:10:40 -0300

    :recycle: chore: refactor code, sort imports and remove unnecessary color closes

--- src/commands/create.py
+++ src/commands/create.py
@@ -1,4 +1,6 @@
-import click, validators, datetime
+import click
+import validators
+import datetime
 from rich import print
 from components.icons import Icon
 from infra.db import Anime, db
@@ -9,21 +11,22 @@ from infra.db import Anime, db
 def create(title: str, url: str):
   """Create an anime with a given title/description"""
   if not validators.url(url):
-    print('{} [bold red]The URL is not valid[/bold red]'.format(Icon.MINUS.value))
+    print('{} [bold red]The URL is not valid'.format(Icon.MINUS.value))
     exit(0)
 
   animeAlreadyExists = Anime.get_or_none(Anime.title == title)
 
   if animeAlreadyExists:
-    print('{} [bold red]{} already exists[/bold red]'.format(Icon.MINUS.value, title))
+    print('{} [bold red]{} already exists.'.format(Icon.MINUS.value, title))
+    print('[red]Exiting...')
     exit(0)
 
-  print('{} Title: [orange_red1]{}[/orange_red1]'.format(Icon.PLUS.value, title))
-  print('{} URL: [orange_red1]{}[/orange_red1]'.format(Icon.PLUS.value, url))
+  print('{} Title: [orange_red1]{}'.format(Icon.PLUS.value, title))
+  print('{} URL: [orange_red1]{}'.format(Icon.PLUS.value, url))
 
   Anime.create(title=title, url=url, created_at=datetime.datetime.now())
 
-  print('[green]Done![/green]')
-  print('[green]The anime {} has been created in the database[/green]'.format(title))
+  print('[green]Done!')
+  print(f'[green]The anime {title} has been created in the database')
 
   db.close()
